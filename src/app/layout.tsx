1c60036ba8ccee02d295be9054e950d5ec9336c2
juanranzon <162765402+juanranzon@users.noreply.github.com>
2025-08-14 17:38:15 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
+import "./globals.css";
 import type { ReactNode } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
