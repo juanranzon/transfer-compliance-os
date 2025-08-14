f670f6054e09496627303ab2c26d71f568e84da5
juanranzon <162765402+juanranzon@users.noreply.github.com>
2025-08-14 17:37:09 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
-export default function RootLayout({ children }) {
+import type { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+
   return (
     <html lang="es">
       <body>{children}</body>
