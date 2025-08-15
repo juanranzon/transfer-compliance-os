74a9096b204946b1da32a58ae1b4f2ec7f7cd28f
juanranzon <162765402+juanranzon@users.noreply.github.com>
2025-08-15 00:53:33 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import UploadForm from '@/components/UploadForm';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+import SolidarityForm from '@/components/forms/SolidarityForm';
 
 export default function Home() {
   return (
     <div className="bg-gray-50 min-h-screen flex flex-col">
       <Header />
       <main className="flex-1">
-        <UploadForm />
+        <SolidarityForm />
       </main>
       <Footer />
     </div>
